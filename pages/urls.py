dffa481b19a89d3a6867ed8e7e628218c19e3da7
Joao Marcos <joaomarcosnina@gmail.com>
2021-01-23 17:06:35 -0300

    Add templete home-home

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,6 @@ from .views import Home, Home_home
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('', Home_home.as_view(), name='home-home')
+    path('home/', Home_home.as_view(), name='home-home')
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,15 @@
 from django.views.generic import TemplateView
 
+from django.urls import reverse_lazy
+
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 class Home(TemplateView):
     template_name = "pages/index.html"
 
 
-class Home_home(TemplateView):
+class Home_home(LoginRequiredMixin,TemplateView):
+    login_url = reverse_lazy('login')
     template_name = "pages/home-home.html"
