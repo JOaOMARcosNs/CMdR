e1f35c1375c9bf714ff421cd65fa180541af686a
Joao Marcos <joaomarcosnina@gmail.com>
2021-01-26 09:26:19 -0300

    Alter .gitignore

--- register/forms.py
+++ register/forms.py
@@ -2,13 +2,16 @@ from django import forms
 
 from .models import Homeless
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class HomelessForm(forms.ModelForm):
     class Meta:
         model = Homeless
         fields = '__all__'
-
+        widgets = {'birth_date': DateInput()}
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['cpf'].widget.attrs.update({'class' : 'mask-cpf'})
         self.fields['rg'].widget.attrs.update({'class' : 'mask-rg'})
-        self.fields['birth_date'].widget.attrs.update({'class' : 'mask-birth_date'})
+        
--- static/js/mask.js
+++ static/js/mask.js
@@ -1,6 +1,5 @@
 $(function(){
 $('.mask-cpf').mask('000.000.000-00', {reverse: true});
-// $('.mask-birth_date').mask('0000/00/00', {reverse: true});
 $('.mask-rg').mask('00.000.000-0', {reverse: true});
 });
 
