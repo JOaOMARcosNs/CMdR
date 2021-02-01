2ba670f2a3131b4caeb71dc8e6085e85750437bc
Joao Marcos <joaomarcosnina@gmail.com>
2021-01-31 21:53:56 -0300

    Add button cancel

--- register/forms.py
+++ register/forms.py
@@ -10,8 +10,24 @@ class HomelessForm(forms.ModelForm):
         model = Homeless
         fields = 'frist_name','second_name','nickname','birth_date','gender','cpf','rg','issuing_body','height','weight', 'blood_type', 'nationality', 'about'
         widgets = {'birth_date': DateInput()}
+
+        # widgets = {
+        #     'name': forms.TextInput(attrs={'placeholder': 'Name'}),
+        #     'description': forms.Textarea(
+        #         attrs={'placeholder': 'Enter description here'}),
+        # }
+        
+        # placeholders = {
+        #     'frist_name': 'awsdasdasdasdasd'
+        # }
+        # labels = {
+        #     'frist_name' : '',
+        # }
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['cpf'].widget.attrs.update({'class' : 'mask-cpf'})
         self.fields['rg'].widget.attrs.update({'class' : 'mask-rg'})
         
+
+
