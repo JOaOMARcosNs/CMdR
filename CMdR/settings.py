6e18d575f0558118aa43bab684e258812adb55ab
Joao Marcos <joaomarcosnina@gmail.com>
2021-02-08 01:08:07 -0300

    Adicionado forgot password

--- CMdR/settings.py
+++ CMdR/settings.py
@@ -140,3 +140,12 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
+
+# config reset password
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'cadastrodemoradoresderua@gmail.com'
+EMAIL_HOST_PASSWORD = 'CMdR11051983'
--- user/urls.py
+++ user/urls.py
@@ -8,9 +8,30 @@ urlpatterns = [
         ), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('registerUser/', UserCreate.as_view(), name='register-user'),
+
     path('chage-password/', MyPasswordChangeView.as_view(), name='password-chage'),
     path('chage-password/done/', MyPasswordChangeDoneView.as_view(), name='password-chage-done'),
+
     path('edit-profile/', UserEditView.as_view(), name='edit-profile'),
+
+    path('reset-password/', auth_views.PasswordResetView.as_view(
+        template_name='user/passord-reset.html'
+    ), name='reset_password'),
+
+    path('reset-password-sent/', auth_views.PasswordResetDoneView.as_view(
+        template_name= 'user/password-reset-sent.html'
+    ), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name= 'user/password-reset-confirm.html'
+    ), name='password_reset_confirm'),
+
+    path('reset-password-complete/', auth_views.PasswordResetView.as_view(
+        template_name= 'user/password-reset-done.html'
+    ), name='password_reset_complete'),
+
+
+    
     
     
 ]
